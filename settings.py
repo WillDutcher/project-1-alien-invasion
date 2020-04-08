41937d1fd4b96187eed3f0386df53f30494dfe3a
Will Dutcher <William.C.Dutcher@gmail.com>
2020-04-08 08:42:39 -0400

    Update settings module.

--- settings.py
+++ settings.py
@@ -7,5 +7,5 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200                                                # Changed from 1200
-        self.screen_height = 800                                               # Changed from 800
+        self.screen_height = 800                                                # Changed from 800
         self.bg_color = pygame.Color('#E6E6E6')                                 # Changed from (230, 230, 230)
