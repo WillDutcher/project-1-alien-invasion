4999cd9078df55c32306b0d1073fc562482dd49c
Will Dutcher <William.C.Dutcher@gmail.com>
2020-04-08 07:05:28 -0400

    Move settings to new file and call from main file.

---
+++ settings.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1920                                                # Changed from 1200
+        self.screen_height = 1080                                               # Changed from 800
+        self.bg_color = pygame.Color('#E6E6E6')                                 # Changed from (230, 230, 230)
