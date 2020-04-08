d3b6a936d54fcae766e1e19e8835d61f31635452
Will Dutcher <William.C.Dutcher@gmail.com>
2020-04-08 09:11:34 -0400

    Allow continuous movement to right; stop on KEYUP event.

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,6 +23,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()                                                # Calls helper method
+            self.ship.update()                                                  # Updates ship's position based on user events
             self._update_screen()                                               # Calls helper method
 
     def _check_events(self):
@@ -32,8 +33,10 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
-                    # Move the ship to the right.
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update images on screen and flip to show newest screen."""
