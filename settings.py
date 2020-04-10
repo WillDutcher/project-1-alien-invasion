3f1ba080e08a551fb8fca1aa3984f053d1ec67f3
Will Dutcher <William.C.Dutcher@gmail.com>
2020-04-10 09:40:18 -0400

    Increase difficulty on cleared levels.

--- settings.py
+++ settings.py
@@ -4,29 +4,24 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         # Screen settings
         self.screen_width = 1200                                                # Changed from 1200
         self.screen_height = 800                                                # Changed from 800
         self.bg_color = pygame.Color('#E6E6E6')                                 # Changed from (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 3
         self.ship_limit = 3
         self.height_limit = self.screen_height * 0.75
 
         # Bullet settings
-        self.bullet_speed = 3
-        self.bullet_width = 3000
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
 
         # Alien settings
-        self.alien_speed = 12
         self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
 
         # Sound settings
         self.hit = pygame.mixer.Sound('sounds/boom.wav')
@@ -38,3 +33,22 @@ class Settings:
         self.round_three = pygame.mixer.Sound('sounds/round_three.wav')
         self.welcome = pygame.mixer.Sound('sounds/welcome.wav')
         self.game_over = pygame.mixer.Sound('sounds/game_over.wav')
+
+        # Difficulty settings
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+    def increase_difficulty(self):
+        """Increase speed settings when player clears board."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
