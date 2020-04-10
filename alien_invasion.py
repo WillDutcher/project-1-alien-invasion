ddd79077819626223ce83d1a3833453e7f6627d8
Will Dutcher <William.C.Dutcher@gmail.com>
2020-04-10 07:31:07 -0400

    Set game_active state; freeze (end) game when ships left == 0.

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,9 +36,12 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()                                                # Calls helper method
-            self.ship.update()                                                  # Updates ship's position based on user events
-            self._update_bullets()                                              # Updates the bullet's position when fired
-            self._update_aliens()                                               # Updates the alien fleet
+
+            if self.stats.game_active:
+                self.ship.update()                                              # Updates ship's position based on user events
+                self._update_bullets()                                          # Updates the bullet's position when fired
+                self._update_aliens()                                           # Updates the alien fleet
+
             self._update_screen()                                               # Calls helper method
 
     def _check_events(self):
@@ -112,19 +115,22 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
 
-        # Decrement ships_left.
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Decrement ships_left.
+            self.stats.ships_left -= 1
 
-        # Get rid of any remaining aliens and bullets.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Create a enw fleet and center the ship.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a enw fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pause.
-        sleep(0.5)
+            # Pause.
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
